5fba81f97f75dcb05d4cf96fd0c34705a8ede8f6
fhx <fhx4850@gmail.com>
2022-02-01 12:06:34 +0200

    update home template

--- static/js/script.js
+++ static/js/script.js
@@ -21,5 +21,55 @@ $(document).ready(function(){
 			}
 		]
 	});
+	document.getElementById('view_all_categories_btn').onclick = function(){
+		let alert = document.getElementById('home_alert_place');
+		showElement(alert);
+		disableScrolling();
+	}
+	document.getElementById('close_alert').onclick = function(){
+		let alert = document.getElementById('home_alert_place');
+		hideElement(alert);
+		enableScrolling();
+	}
+	document.getElementById('home-alert-bg').onclick = function(){
+		let alert = document.getElementById('home_alert_place');
+		hideElement(alert);
+		enableScrolling();
+	}
+
+	if(document.getElementsByClassName('like_btn')){
+		let q = document.getElementsByClassName('like_btn');
+		for(let i = 0; i < q.length; i++){
+			q[i].onclick = function(){
+				like(q[i]);
+			}
+		}
+	}
 });
 
+function disableScrolling(){
+	let scroll_y=window.scrollY;
+	let scroll_x=window.scrollX;
+	window.onscroll=function(){
+		window.scrollTo(scroll_x, scroll_y);
+	};
+}
+
+function enableScrolling(){
+	window.onscroll=function(){};
+}
+
+function showElement(elem){
+	elem.style.opacity = '1';
+	elem.style.visibility = 'visible';
+}
+
+function hideElement(elem){
+	elem.style.opacity = '0';
+	elem.style.visibility = 'hidden';
+}
+
+function like(elem){
+	elem.classList.toggle('btn_red');
+	// elem.style.fill = '#000';
+}
