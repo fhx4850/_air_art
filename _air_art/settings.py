2f74c8ababbd06ed11fa0daa88d060ec428441f6
fhx <fhx4850@gmail.com>
2022-02-18 09:50:01 +0200

    switched to mysql db

--- _air_art/settings.py
+++ _air_art/settings.py
@@ -78,10 +78,20 @@ WSGI_APPLICATION = '_air_art.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '_air_art',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
     }
 }
 
